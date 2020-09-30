2284f4ac73c3533322182e546948b118bcf7cc07
sirlocust <jua56yu@hotmail.com>
2020-09-29 19:43:22 -0500

    se crea proyecto en webpack para hacer aplicacion todo con javascript vanilla

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 4210,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin()
+  ],
+};
